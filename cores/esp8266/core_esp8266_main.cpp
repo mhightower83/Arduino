051788328dad0dafc295a933e49c89f364d762ec
M Hightower <27247790+mhightower83@users.noreply.github.com>
2020-11-11 22:19:55 -0800

    Handle breakpoints via postmortem when GDB is not running.
    In the absence of GDB, install a small breakpoint handler that clears EXCM and
    executes an illegal instruction. This leverages the existing exception
    handling code in the SDK for `ill` instruction. At postmortem processing,
    the real event is identified and reported. This allows for alerting of embedded
    breakpoints. In the stack trace, `epc1` is updated with the value of `epc2` so
    that the "ESP Exception Decoder" will identify the line where the BP occurred.
    
    The SDKs default behavior was to loop on breakpoints waiting for interrupts
    >2 until the HWDT kicks in. The current "C++" compiler will embed breakpoints
    when it detects a divide by zero at compile time.
    
    Expanded technique to preserve more of the current state and to also report
    Kernel and Double exceptions through postmortem.

--- cores/esp8266/core_esp8266_main.cpp
+++ cores/esp8266/core_esp8266_main.cpp
@@ -392,6 +392,7 @@ extern "C" void __disableWiFiAtBootTime (void)
     wifi_fpm_open();
     wifi_fpm_do_sleep(0xFFFFFFF);
 }
+extern "C" void postmortem_init(void);
 
 extern "C" void user_init(void) {
     struct rst_info *rtc_info_ptr = system_get_rst_info();
@@ -422,6 +423,11 @@ extern "C" void user_init(void) {
 #if defined(MMU_IRAM_HEAP)
     umm_init_iram();
 #endif
+
+#if defined(DEBUG_ESP_PORT) || defined(DEBUG_ESP_EXCEPTIONS)
+    postmortem_init();
+#endif
+
     preinit(); // Prior to C++ Dynamic Init (not related to above init() ). Meant to be user redefinable.
     __disableWiFiAtBootTime(); // default weak function disables WiFi
 
